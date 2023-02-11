e2b9dc52836d09ff0346373d1b67dee90e7c5ad7
jewoodev <jewoos15@naver.com>
2023-02-11 20:09:22 +0900

    After model tuning

--- 110~/evaluate_model.py
+++ 110~/evaluate_model.py
@@ -7,8 +7,8 @@ def evaluate_score(y_test, y_pred):
     rmse = np.sqrt(mse)
     r2 = r2_score(y_test, y_pred)
 
-    # print(f'mse: {mse:.3f}')
-    # print(f'rmse: {rmse:.3f}')
-    # print(f'r2: {r2:.3f}')
+    print(f'mse: {mse:.3f}')
+    print(f'rmse: {rmse:.3f}')
+    print(f'r2: {r2:.3f}')
     
     return mse, rmse, r2
